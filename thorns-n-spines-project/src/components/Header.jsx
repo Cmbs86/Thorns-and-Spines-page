661d486f45781f2a1c7ef9b3ac58f46ca9fca581
Cmbs86 <carlosmbs86@outlook.ie>
2024-09-10 20:02:29 +0200

    add burgermenu

--- thorns-n-spines-project/src/components/Header.jsx
+++ thorns-n-spines-project/src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import HamburgerMenu from './MenuHB/HamburgerMenu'
 
 const Header = () => {
   return (
+
+    <>
+    <HamburgerMenu /> 
     <h1>Welcome to Thorns and Spines</h1>
+    </>
   )
 }
 
