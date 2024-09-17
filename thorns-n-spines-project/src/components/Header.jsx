046b2d294deefe5cdf6de9a947d77689adce5d27
Cmbs86 <carlosmbs86@outlook.ie>
2024-09-17 20:54:40 +0200

    Remove HamburgerMenu component from Header.jsx

--- thorns-n-spines-project/src/components/Header.jsx
+++ thorns-n-spines-project/src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import HamburgerMenu from './MenuHB/HamburgerMenu'
+
 
 const Header = () => {
   return (
 
     <>
-    <HamburgerMenu /> 
     <h1 className="text-3xl font-bold underline">Welcome to Thorns and Spines</h1>
     </>
   )
