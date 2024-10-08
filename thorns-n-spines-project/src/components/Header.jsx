30b0a9597880d9646b0a8d12c8d6c3458d68a4d3
Cmbs86 <carlosmbs86@outlook.ie>
2024-10-08 12:30:10 +0200

    change navmobile styles cover on toggle

--- thorns-n-spines-project/src/components/Header.jsx
+++ thorns-n-spines-project/src/components/Header.jsx
@@ -5,7 +5,6 @@ const Header = () => {
   return (
     <div className=" w-full h-auto fixed top-0 left-0 right-0 bg-tns-ash-gray-light  text-tns-ebony  border-x-tns-mint-cream font-sans z-50">
       <nav className="w-full flex items-center justify-start py-3 lg:py-8">
-        {/* <span className="text-lg">⚡️</span> */}
         <NavMobile />
         <NavDesktop />
       </nav>
--- thorns-n-spines-project/src/components/NavMobile.jsx
+++ thorns-n-spines-project/src/components/NavMobile.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Squash as Hamburger } from "hamburger-react";
 import { routes } from "../routes";
-import Hero from "../components/Hero.jsx";
+
 
 
 const NavMobile = () => {
@@ -14,7 +14,7 @@ const NavMobile = () => {
   useClickAway(ref, () => setOpen(false));
 
   return (
-    <div ref={ref} className="lg:hidden w-full ml-5 mt-5 flex justify-start border border-solid border-yellow-500">
+    <div ref={ref} className="lg:hidden  w-full pl-2 mt-5 flex ">
       <Hamburger toggled={isOpen} size={40} toggle={setOpen} />
       <AnimatePresence>
         {isOpen && (
@@ -23,9 +23,9 @@ const NavMobile = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.2 }}
-            className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0"
+            className="flex w-full h-full pb-96"
           >
-            <ul className="grid gap-2 mb-32 mt-48 ">
+            <ul className="w-full flex flex-col m-0 p-0 mt-48">
               {routes.map((route, idx) => {
                 const { Icon } = route;
                 
@@ -40,17 +40,18 @@ const NavMobile = () => {
                     delay: 0.1 + idx / 10,
                   }}
                   key={route.title}
-                  className="w-full p-[0.08rem] py-2 rounded-xl "
+                  className="flex w-full py-2  "
                   >
                 
                     <a
                       onClick={() => setOpen((prev) => !prev)}
                       className={
-                        "flex items-center justify-center w-full rounded-xl"
+                        "flex items-center justify-center w-full"
                       }
                       href={route.href}
                       >
-                      <span className="flex gap-1 text-4xl mb-20 font-bold text-tns-mint-cream">{route.title}</span>
+                        {/* fix media queries of the link/routes */}
+                      <span className="flex text-4xl mb-20 font-bold text-tns-mint-cream">{route.title}</span>
                       {/* <Icon className="text-xl" /> */}
                     </a>
                   </motion.li>
