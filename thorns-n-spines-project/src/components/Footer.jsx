375d77557fc1151f30117c77a53b52044f836408
Cmbs86 <carlosmbs86@outlook.ie>
2024-10-10 12:20:06 +0200

    fix footer and Hero

--- thorns-n-spines-project/src/components/Footer.jsx
+++ thorns-n-spines-project/src/components/Footer.jsx
@@ -1,26 +1,30 @@
 const Footer = () => {
   return (
-    <footer className="font-sans text-xl  text-tns-ebony border border-solid border-black z-50">
-      <div className=" border border-solid border-red-500">
-        <div className=" flex flex-col w-1/2 h-full justify-center items-center border border-solid border-red-500 ">
-          <div className="flex h-1/2 items-start">
-            <h3 className="text-3xl font-bold border-b-2 border-tns-ebony ">
+    <footer className="w-full ">
+      <div className=" font-sans text-xl flex flex-wrap w-full py-11 md:max-w-[1600px] gap-4 md:gap-0  mx-auto justify-center  h-full  text-tns-ebony">
+        <div className=" flex flex-col flex-wrap w-1/2 gap-4  justify-center items-center  ">
+          <div className="flex items-start">
+            <h3 className="text-2xl md:text-3xl font-bold border-b-2 border-tns-ebony ">
               Location
             </h3>
           </div>
-          <div className="flex h-1/2 items-end">
+          <div className="flex text-center  text-sm md:text-xl ">
             <p>Dreilindenstraße 25, 04177 Leipzig</p>
           </div>
         </div>
-        <div className="flex flex-col w-1/2 justify-center items-center border border-solid border-red-500 ">
-          <h3 className="text-3xl font-bold border-b-2 border-tns-ebony ">
-            Contact
-          </h3>
-          <p>contact@thornsandspines.de</p>
+        <div className=" flex flex-col w-1/2 flex-wrap gap-4  justify-center items-center  ">
+          <div className="flex items-start">
+            <h3 className="text-2xl md:text-3xl font-bold border-b-2 border-tns-ebony ">
+              Contact
+            </h3>
+          </div>
+          <div className="flex text-center text-sm md:text-xl break-all ">
+            <p>contact@thornsandspines.de</p>
+          </div>
         </div>
       </div>
-      <div className="flex h-full justify-center items-end border border-solid border-red-500">
-        <p className="">
+      <div className="flex pt-3 pr-2 text-center text-tns-ebony font-sans border-t border-tns-ebony ">
+        <p className="flex w-full justify-end ">
           &copy; {new Date().getFullYear()} Thorns and Spines. All Rights
           Reserved.
         </p>
--- thorns-n-spines-project/src/components/Hero.jsx
+++ thorns-n-spines-project/src/components/Hero.jsx
@@ -2,23 +2,26 @@ import ThornsHero from "../assets/images/hero.webp";
 
 const Hero = () => {
   return (
-    <div className="fixed w-full h-[80vh] ">
+    <div className="relative w-full h-[80vh] z-10">
       {/* Background image container */}
       <div
-        className="absolute w-full h-full bg-cover bg-center bg-no-repeat bg-fixed"
-        style={{ backgroundImage: `url(${ThornsHero})` }}
+        className="absolute w-full h-full"
+        style={{ backgroundImage: `url(${ThornsHero})`,  backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed", }}
       >
         {/* Container for the hero content */}
-        <div className=" w-full h-full flex flex-col justify-between  ">
-          
+        {/* <div className=" w-full h-full flex flex-col justify-between  ">
+           */}
           {/* Top animated heading */}
-          <div className="absolute bottom-10 w-full">
+          {/* <div className="absolute bottom-10 w-full">
             <h1 className="text-5xl md:text-6xl font-bold animate-marquee w-full text-center text-tns-ash-gray-light font-sans">
               Welcome to Thorns and Spines
             </h1>
-          </div>
+          </div> */}
           
-        </div>
+        {/* </div> */}
       </div>
     </div>
     /* Work on the positioning here: Change absolute, fixed and relative. This might fix the problem with the footer, and the "hidden" on media queries. */
