c8163afd413cc628f57a0778c27f975a35651c18
Cmbs86 <carlosmbs86@outlook.ie>
2024-10-10 15:13:58 +0200

    add useEffect Navmobile overflow

--- thorns-n-spines-project/src/components/Header.jsx
+++ thorns-n-spines-project/src/components/Header.jsx
@@ -3,7 +3,7 @@ import NavMobile from "./NavMobile";
 
 const Header = () => {
   return (
-    <div className="  bg-tns-ash-gray-light  text-tns-ebony  border-x-tns-mint-cream font-sans ">
+    <div className=" bg-tns-ash-gray-light  text-tns-ebony  border-x-tns-mint-cream font-sans ">
       <nav className="w-full flex items-center justify-start py-3 md:py-8">
         <NavMobile />
         <NavDesktop />
