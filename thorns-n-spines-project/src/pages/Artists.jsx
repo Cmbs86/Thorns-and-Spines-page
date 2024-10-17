3e3d2f5723ca5f06fe50a9c56dd3572da84f28db
Cmbs86 <carlosmbs86@outlook.ie>
2024-10-17 12:12:54 +0200

    fix card sizes & picture format

--- thorns-n-spines-project/src/pages/Artists.jsx
+++ thorns-n-spines-project/src/pages/Artists.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SeppTattoo from "../assets/images/sepp.webp";
 import gabitattoo from "../assets/images/gabi.webp";
 import maikontattoo from "../assets/images/maikon.webp";
+import mariatattoo from "../assets/images/maria.webp"
 
 
 const Artists = () => {
@@ -9,7 +10,7 @@ const Artists = () => {
     <>
       <div className=" min-h-screen fle flex-col items-center justify-center">
         {/* Text section */}
-        <div className="text-center mb-8">
+        <div className="text-center my-8">
           <h1 className="text-4xl lg:text-6xl font-bold text-tns-ebony font-sans ">Resident Artists</h1>
         
         </div>
@@ -19,25 +20,25 @@ const Artists = () => {
         <div className="max-w-[1200px] w-full mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8">
           {/* Individual Cards */}
           <div className="p-6 rounded-lg text-center">
-            <img src={SeppTattoo} alt="" className="rounded-full w-64 h-64 object-cover mx-auto"  />
+            <img src={SeppTattoo} alt="" className="rounded-full w-[475px] h-[475px] object-cover mx-auto"  />
             <h3 className="mt-8 text-5xl font-bold font-sans text-tns-ebony">Sep</h3>
             <p className="text-xl text-tns-ebony">@sep</p>
             <a href="#book-artist" className="mt-2 text-tns-ebony ">Book via link</a>
           </div>
           <div className="p-6 rounded-lg text-center">
-            <img src={gabitattoo} alt="" className="rounded-full w-64 h-64 object-cover mx-auto" />
+            <img src={gabitattoo} alt="" className="rounded-full w-[475px] h-[475px] object-cover mx-auto" />
             <h3 className="mt-8 text-5xl font-bold font-sans text-tns-ebony">Gabi</h3>
             <p className="text-xl text-tns-ebony">@artistone</p>
             <a href="#book-artist" className="mt-2 text-tns-ebony">For bookings click here</a>
           </div>
           <div className="p-6 rounded-lg text-center">
-            <img src={maikontattoo} alt="" className="rounded-full w-64 h-64 object-cover mx-auto" />
+            <img src={maikontattoo} alt="" className="rounded-full w-[475px] h-[475px] object-cover mx-auto" />
             <h3 className="mt-8 text-5xl font-bold font-sans text-tns-ebony">Artist1</h3>
             <p className="text-xl text-tns-ebony">@artistone</p>
             <a href="#book-artist" className="mt-2 text-tns-ebony">Book via link</a>
           </div>
           <div className="p-6 rounded-lg text-center">
-            <img src="" alt="" className="rounded-full w-64 h-64 object-cover mx-auto" />
+            <img src={mariatattoo} alt="" className="rounded-full w-[474px] h-[474px] object-cover mx-auto" />
             <h3 className="mt-4 text-xl font-bold font-sans">Artist1</h3>
             <p className="text-sm">@artistone</p>
             <a href="#book-artist" className="mt-2 text-blue-500 underline">Book via link</a>
