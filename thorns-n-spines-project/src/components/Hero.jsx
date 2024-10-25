0d99880aac7dc5c67bf4d705c04b41e0d246468e
Cmbs86 <carlosmbs86@outlook.ie>
2024-10-25 20:31:17 +0200

    add animation hero

--- thorns-n-spines-project/src/components/Hero.jsx
+++ thorns-n-spines-project/src/components/Hero.jsx
@@ -1,28 +1,34 @@
+import { motion } from "framer-motion";
 import ThornsHero from "../assets/images/hero.webp";
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-[80vh] z-0 ">
+    <div className="relative w-full h-[80vh] z-0 overflow-hidden ">
       {/* Background image container */}
-      <div
+      <motion.div
         className="absolute w-full h-full"
-        style={{ backgroundImage: `url(${ThornsHero})`,  backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundAttachment: "fixed", }}
+        style={{
+          backgroundImage: `url(${ThornsHero})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+        }}
+        initial={{ scale: 1.1 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 1.5, ease: "easeOut" }}
+      ></motion.div>
+      {/* Container for the hero content */}
+      <motion.div
+        className="relative flex justify-center items-center h-full"
+        initial={{ opacity: 0, y: 20}}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.5, delay: 0.5 }}
       >
-        {/* Container for the hero content */}
-        {/* <div className=" w-full h-full flex flex-col justify-between  ">
-           */}
-          {/* Top animated heading */}
-          {/* <div className="absolute bottom-10 w-full">
-            <h1 className="text-5xl md:text-6xl font-bold animate-marquee w-full text-center text-tns-ash-gray-light font-sans">
-              Welcome to Thorns and Spines
-            </h1>
-          </div> */}
-          
-        {/* </div> */}
-      </div>
+        <h1 className="text-5xl md:text-6xl font-bold  text-tns-ash-gray-light font-sans">
+          Welcome to Thorns and Spines
+        </h1>
+      </motion.div>
     </div>
     /* Work on the positioning here: Change absolute, fixed and relative. This might fix the problem with the footer, and the "hidden" on media queries. */
   );
