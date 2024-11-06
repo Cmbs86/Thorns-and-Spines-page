328fe760b8bb0998328aa822cce8db63dabdab69
Cmbs86 <carlosmbs86@outlook.ie>
2024-11-06 20:28:14 +0100

    media queries contact.jsx

--- thorns-n-spines-project/src/pages/Contact.jsx
+++ thorns-n-spines-project/src/pages/Contact.jsx
@@ -1,21 +1,22 @@
+import AnimatedSeparator from "../components/AnimatedSeparator.jsx";
 import ContentLayout from "../layouts/ContentLayout.jsx";
 import { useTranslation } from "react-i18next";
-import {useState} from "react";
+
 const Contact = () => {
   const { t } = useTranslation();
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  
   return (
     <>
     <ContentLayout>
       <main className="min-h-screen w-full flex flex-col items-center">
-        <div className="text-center my-10">
+        <div className="text-center mt-10 mb-10  md:mb-24">
           <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold text-tns-ebony font-sans  ">
             {t("contact")}
           </h1>
         </div>
-        <div className="w-full mx-auto flex flex-col md:flex-row flex-wrap justify-around 2xl:justify-between gap-y-10"></div>
-        <div className="flex gap-20 w-full justify-center">
-          <div className="w-[750px] h-[750px] rounded-2xl overflow-hidden border-tns-ebony border ">
+      
+        <div className="flex flex-col flex-wrap lg:flex-row gap-12 w-full justify-around">
+          <div className="flex flex-col lg:flex-row w-[350px] h-[280px] md:w-[450px] md:h-[380px] lg:w-[850px] lg:h-[550px]  border-tns-ebony border  ">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2492.6304376548464!2d12.336780076811534!3d51.33631692318685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a6f7f3b8c9cf13%3A0x348b2585962a6112!2sThorns%20and%20Spines!5e0!3m2!1sen!2sde!4v1730383847564!5m2!1sen!2sde"
             width="100%"
@@ -25,19 +26,28 @@ const Contact = () => {
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
           </div>
-          <div className="flex font-sans text-tns-ebony font-bold">
-            <div className="flex flex-col h-full justify-center items-center">
-            <div className="flex gap-y-10 text-center">
-              <h3 className="text-4xl">Our Address</h3>
+
+          <div className="flex flex-col font-sans text-tns-ebony font-bold">
+            <div className="flex flex-col gap-y-10  justify-center items-center">
+            <div className="w-full">
+              <h3 className="text-3xl md:text-4xl lg:text-6xl text-center lg:text-start">Our Address:</h3>
             </div>
-              <h1 className="text-6xl">
+              <h1 className="text-3xl md:text-4xl lg:text-6xl">
                 Dreilindenstraße 25,
                 <span className="block mt-1">04177</span>
                 <span className="block mt-1">Leipzig</span>
               </h1>
+              
+                <div className="max-w-[600px]">
+               <p className="lg:text-xl p-2 items-center text-center  ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptatum commodi doloribus consequatur est quis, esse consequuntur suscipit voluptas adipisci placeat similique nam quaerat libero quod omnis tempora quia facilis provident nesciunt dolorem perferendis quae delectus. Nihil soluta, iusto pariatur voluptas ex animi recusandae aliquam. Rerum quos pariatur expedita cum?</p>
+               </div>
+             
             </div>
+            
           </div>
+
         </div>
+        <AnimatedSeparator />
       </main>
       </ContentLayout>
     </>
