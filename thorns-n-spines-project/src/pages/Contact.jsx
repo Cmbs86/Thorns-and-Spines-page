93e6e448ea89e935df2114a12ef9e86629012eb6
Cmbs86 <carlosmbs86@outlook.ie>
2024-11-15 15:52:50 +0100

    fix media queries contact page && add useMediaQuery custom hook.

--- thorns-n-spines-project/src/pages/Contact.jsx
+++ thorns-n-spines-project/src/pages/Contact.jsx
@@ -6,6 +6,7 @@ const Contact = () => {
   const { t } = useTranslation();
 
   const isBelowLg = useMediaQuery("(max-width:1024px)");
+
   return (
     <>
       <ContentLayout>
@@ -16,8 +17,8 @@ const Contact = () => {
             </h1>
           </div>
           {/* map container */}
-          <div className="flex flex-wrap gap-12  w-full justify-around items-center">
-            <div className="flex  w-[450px] h-[280px] sm:w-[550px] sm:h-[380px]  md:w-[650px] md:h-[480px] lg:w-[750px] lg:h-[500px]  border-tns-ebony border  ">
+          <div className={`flex ${isBelowLg ? "flex-col" : "flex-row"} gap-8 px-8  w-full justify-around items-center`}>
+            <div className="flex w-[350px] h-[200px] sm:w-[450px] sm:h-[280px] md:w-[550px] md:h-[380px] lg:w-[650px] lg:h-[480px] border-tns-ebony border">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2492.6304376548464!2d12.336780076811534!3d51.33631692318685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a6f7f3b8c9cf13%3A0x348b2585962a6112!2sThorns%20and%20Spines!5e0!3m2!1sen!2sde!4v1730383847564!5m2!1sen!2sde"
                 width="100%"
@@ -31,35 +32,24 @@ const Contact = () => {
             <div className="flex flex-col flex-wrap font-sans text-tns-ebony font-bold">
               <section className="flex flex-col gap-y-10  justify-center items-center">
                 <div className="w-full">
-                  <h3 className="text-3xl md:text-4xl lg:text-6xl text-center lg:text-start">
+                  <h3 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center lg:text-start">
                    {t("our_address" )}
                   </h3>
                 </div>
                 {/* change mb-10 here for more if needed and check animated separator on media queries */}
-                <h1 className="mb-12 text-3xl md:text-4xl lg:text-6xl">
+                <h1 className="mb-12 text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
                   Dreilindenstraße 25,
                   <span className="block mt-1">04177</span>
                   <span className="block mt-1">Leipzig</span>
                 </h1>
 
-                {/* <div className="max-w-[600px]">
-                  <p className="lg:text-xl py-2 pl-1 text-center lg:text-start font-sans   ">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Possimus voluptatum commodi doloribus consequatur est quis,
-                    esse consequuntur suscipit voluptas adipisci placeat
-                    similique nam quaerat libero quod omnis tempora quia facilis
-                    provident nesciunt dolorem perferendis quae delectus. Nihil
-                    soluta, iusto pariatur voluptas ex animi recusandae aliquam.
-                    Rerum quos pariatur expedita cum?
-                  </p>
-                </div> */}
               </section>
             </div>
             {isBelowLg &&  <AnimatedSeparator /> }
          
           </div>
           {/* Contact form container */}
-          <div className="flex flex-wrap gap-12 w-full my-20 justify-around items-center">
+          <div className={`flex ${isBelowLg ? "flex-col" : "flex-row"} my-20 gap-8 px-8  w-full justify-around items-center`}>
             <div className="flex flex-wrap  text-tns-ebony lg:w-[750px] lg:h-[500px]    ">
               <div className="w-full text-center">
                 <h1 className="text-3xl md:text-4xl lg:text-6xl text-center  font-sans font-bold mb-10 lg:mb-0 mt-5">{t("contact_us")}</h1>
