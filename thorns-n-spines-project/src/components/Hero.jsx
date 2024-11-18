c2166ce9f3aee2b69e12196b977647f2e131185c
Cmbs86 <carlosmbs86@outlook.ie>
2024-11-18 17:43:19 +0100

    fix animation duration hero.jsx

--- thorns-n-spines-project/src/components/Hero.jsx
+++ thorns-n-spines-project/src/components/Hero.jsx
@@ -19,22 +19,22 @@ const Hero = () => {
           backgroundRepeat: "no-repeat",
         
         }}
-        initial={{ scale: 1.1 }}
+        initial={{ scale: 1.3 }}
         animate={{ scale: 1 }}
-        transition={{ duration: 2, ease: "easeOut" }}
+        transition={{ duration: 4, ease: "easeOut" }}
       />
      <motion.div
         className="absolute inset-0 bg-gradient-to-b from-transparent to-black/70"
         initial={{ opacity: 0 }}
-        animate={{ opacity: 0.9 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 2, ease: "easeInOut" }}
       />
       {/* Container for the hero content */}
       <motion.div
         className="relative flex justify-center text-center items-end pb-14  sm:pb-28 h-full"
-        initial={{ opacity: 0, y: 20}}
+        initial={{ opacity: 0, y: 50}}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 3, delay: 0.5 }}
+        transition={{ duration: 4.8, delay: 0.7 }}
       >
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl  font-bold  text-tns-ash-gray-light font-sans">
          {t("welcome")}
