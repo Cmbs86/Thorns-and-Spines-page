cc459b890c3788d393a819ae9fdfd84b167e5cef
Cmbs86 <carlosmbs86@outlook.ie>
2024-11-18 20:35:36 +0100

    add few adjustments and comments

--- thorns-n-spines-project/src/main.jsx
+++ thorns-n-spines-project/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import '../src/i18m.js'; // Import i18n configuration here
+import '../src/i18m.js'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
