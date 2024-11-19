928dfd395e25f4905263ab0d2ddf8c7f59a70e4b
Cmbs86 <carlosmbs86@outlook.ie>
2024-11-19 15:51:32 +0100

    create ContactForm component && add Emailjs services to contact form

--- thorns-n-spines-project/src/pages/Contact.jsx
+++ thorns-n-spines-project/src/pages/Contact.jsx
@@ -2,6 +2,8 @@ import AnimatedSeparator from "../components/AnimatedSeparator.jsx";
 import ContentLayout from "../layouts/ContentLayout.jsx";
 import { useTranslation } from "react-i18next";
 import useMediaQuery from "../hooks/useMediaQuery.js";
+import ContactForm from "../components/ContactForm.jsx";
+
 const Contact = () => {
   const { t } = useTranslation();
 
@@ -27,9 +29,9 @@ const Contact = () => {
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2492.6304376548464!2d12.336780076811534!3d51.33631692318685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a6f7f3b8c9cf13%3A0x348b2585962a6112!2sThorns%20and%20Spines!5e0!3m2!1sen!2sde!4v1730383847564!5m2!1sen!2sde"
                 width="100%"
                 height="100%"
-                allowfullscreen=""
+                allowFullScreen
                 loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"
+               referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
             </div>
             {/* Address info container */}
@@ -76,64 +78,7 @@ const Contact = () => {
               </div>
             </div>
             <div className="max-w-[600px]flex flex-col flex-wrap font-sans text-tns-ebony-dark font-bold">
-              <form className="md:w-[600px]">
-                {/* Name Field */}
-                <div className="">
-                  <label className="block text-tns-ebony-dark text-lg  font-medium mb-2">
-                    Name{" "}
-                  </label>
-                  <div className="flex space-x-4">
-                    <input
-                      type="text"
-                      placeholder={t("first_name")}
-                      required
-                      className="w-full p-3 bg-tns-mint-cream border border-tns-ash-gray-light  focus:outline-none focus:ring-2 focus:ring-tns-ebony"
-                    />
-                    <input
-                      type="text"
-                      placeholder={t("last_name")}
-                      required
-                      className="w-full p-3  bg-tns-mint-cream  border border-tns-ash-gray-light  focus:outline-none focus:ring-2 focus:ring-tns-ebony"
-                    />
-                  </div>
-                </div>
-
-                {/* Email Field */}
-                <div className="mb-6">
-                  <label className="block text-tns-ebony-dark text-lg  font-medium mt-1 mb-2">
-                    Email{" "}
-                  </label>
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    required
-                    className="w-full p-3  bg-tns-mint-cream  border border-tns-ash-gray-light  focus:outline-none focus:ring-2 focus:ring-tns-ebony"
-                  />
-                </div>
-
-                {/* Message Field */}
-                <div className="mb-6">
-                  <label className="block text-tns-ebony-dark text-lg font-medium mb-2">
-                    Message{" "}
-                  </label>
-                  <textarea
-                    placeholder={t("message")}
-                    required
-                    rows="5"
-                    className="w-full p-3 border bg-tns-mint-cream border-tns-ash-gray-light  focus:outline-none focus:ring-2 focus:ring-tns-ebony resize-none"
-                  ></textarea>
-                </div>
-
-                {/* Send Button */}
-                <div className="flex justify-center">
-                  <button
-                    type="submit"
-                    className="px-6 py-3 text-tns-ebony-dark border-2 border-tns-ebony  font-semibold hover:bg-tns-ebony hover:text-tns-mint-cream transition-colors"
-                  >
-                    {t("send")}
-                  </button>
-                </div>
-              </form>
+             <ContactForm />
             </div>
           </div>
           <AnimatedSeparator />
@@ -142,5 +87,64 @@ const Contact = () => {
     </>
   );
 };
+
+// {/* <form className="md:w-[600px]">
+// {/* Name Field */}
+// <div className="">
+//   <label className="block text-tns-ebony-dark text-lg  font-medium mb-2">
+//     Name{" "}
+//   </label>
+//   <div className="flex space-x-4">
+//     <input
+//       type="text"
+//       placeholder={t("first_name")}
+//       required
+//       className="w-full p-3 bg-tns-mint-cream border border-tns-ash-gray-light  focus:outline-none focus:ring-2 focus:ring-tns-ebony"
+//     />
+//     <input
+//       type="text"
+//       placeholder={t("last_name")}
+//       required
+//       className="w-full p-3  bg-tns-mint-cream  border border-tns-ash-gray-light  focus:outline-none focus:ring-2 focus:ring-tns-ebony"
+//     />
+//   </div>
+// </div>
+
+// {/* Email Field */}
+// <div className="mb-6">
+//   <label className="block text-tns-ebony-dark text-lg  font-medium mt-1 mb-2">
+//     Email{" "}
+//   </label>
+//   <input
+//     type="email"
+//     placeholder="Email"
+//     required
+//     className="w-full p-3  bg-tns-mint-cream  border border-tns-ash-gray-light  focus:outline-none focus:ring-2 focus:ring-tns-ebony"
+//   />
+// </div>
+
+// {/* Message Field */}
+// <div className="mb-6">
+//   <label className="block text-tns-ebony-dark text-lg font-medium mb-2">
+//     Message{" "}
+//   </label>
+//   <textarea
+//     placeholder={t("message")}
+//     required
+//     rows="5"
+//     className="w-full p-3 border bg-tns-mint-cream border-tns-ash-gray-light  focus:outline-none focus:ring-2 focus:ring-tns-ebony resize-none"
+//   ></textarea>
+// </div>
+
+// {/* Send Button */}
+// <div className="flex justify-center">
+//   <button
+//     type="submit"
+//     className="px-6 py-3 text-tns-ebony-dark border-2 border-tns-ebony  font-semibold hover:bg-tns-ebony hover:text-tns-mint-cream transition-colors"
+//   >
+//     {t("send")}
+//   </button>
+// </div>
+// </form> */}
 
 export default Contact;
