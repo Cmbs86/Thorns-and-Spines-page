a2d97150b66f87331a770deb5fde4b117bd22968
Cmbs86 <carlosmbs86@outlook.ie>
2024-12-02 19:54:56 +0100

    add active link no hover && hover routes NavDesktop and NavMobile components

--- src/components/NavDesktop.tsx
+++ src/components/NavDesktop.tsx
@@ -1,21 +1,34 @@
 import { useRoutes } from "../routes";
+import { useLocation } from "react-router-dom";
 
 const NavDesktop = () => {
   const routes = useRoutes();
+  const location = useLocation(); // Get the current location
+
   return (
     <ul className="hidden md:flex md:items-center gap-4">
       {routes.map((route) => {
         const { href, title } = route;
+
+        // Check if the current route matches the link's href
+        const isActive = location.pathname === href;
+
         return (
           <li key={title}>
-            <a
-              
-              href={href}
-              className="items-center gap-1 text-tns-ebony hover:text-tns-ash-gray/90  transition-all text-xl font-bold"
-            >
-              {/* <Icon /> */}
-              {title}
-            </a>
+            {isActive ? (
+              <span
+                className="items-center gap-1 text-tns-ebony cursor-default text-xl font-bold transition-all"
+              >
+                {title}
+              </span>
+            ) : (
+              <a
+                href={href}
+                className="items-center gap-1 text-tns-ebony hover:text-tns-ash-gray/90 transition-all text-xl font-bold"
+              >
+                {title}
+              </a>
+            )}
           </li>
         );
       })}
--- src/components/NavMobile.tsx
+++ src/components/NavMobile.tsx
@@ -9,12 +9,13 @@ import { Squash as Hamburger } from "hamburger-react";
 
 //Routes import
 import { useRoutes } from "../routes";
-
+import { useLocation } from "react-router-dom";
 
 const NavMobile = () => {
   const [isOpen, setOpen] = useState<boolean>(false);
   const ref = useRef<HTMLDivElement>(null);
   const routes = useRoutes();
+  const location = useLocation(); // Get the current location
 
   useEffect(() => {
     if (isOpen) {
@@ -32,7 +33,13 @@ const NavMobile = () => {
 
   return (
     <div ref={ref} className="md:hidden w-full mt-5 flex ">
-      <Hamburger toggled={isOpen} size={30} toggle={setOpen} aria-expanded={isOpen ? "true" : "false"} aria-label="Toggle navigation"  />
+      <Hamburger
+        toggled={isOpen}
+        size={30}
+        toggle={setOpen}
+        aria-expanded={isOpen ? "true" : "false"}
+        aria-label="Toggle navigation"
+      />
 
       <AnimatePresence>
         {isOpen && (
@@ -43,9 +50,10 @@ const NavMobile = () => {
             transition={{ duration: 0.2 }}
             className="flex w-full pb-[80vh]  "
           >
-            <ul className="w-[85%] flex flex-col m-0 p-0 mt-24  ">
+            <ul className="w-[85%] flex flex-col m-0 p-0 mt-24">
               {routes.map((route, idx) => {
-                // const { Icon } = route;
+                const { href, title } = route;
+                const isActive = location.pathname === href; // Check if the current route matches the link's href
 
                 return (
                   <motion.li
@@ -57,20 +65,24 @@ const NavMobile = () => {
                       damping: 20,
                       delay: 0.1 + idx / 10,
                     }}
-                    key={route.title}
-                    className="flex py-2  "
+                    key={title}
+                    className="flex py-2"
                   >
-                    <a
-                      onClick={() => setOpen((prev) => !prev)}
-                      className="flex items-center justify-center w-full "
-                      href={route.href}
-                    >
-                      {/* fix media queries of the link/routes */}
-                      <span className="flex text-xl md:text-2xl mb-10 font-bold text-tns-ebony">
-                        {route.title}
+                    {isActive ? (
+                      <span
+                        className="flex items-center justify-center w-full text-xl md:text-2xl mb-10 font-bold text-tns-ebony cursor-default"
+                      >
+                        {title}
                       </span>
-                      {/* <Icon className="text-xl" /> */}
-                    </a>
+                    ) : (
+                      <a
+                        onClick={() => setOpen((prev) => !prev)}
+                        className="flex items-center justify-center w-full text-xl md:text-2xl mb-10 font-bold text-tns-ebony  hover:text-tns-ash-gray/90 transition-all"
+                        href={href}
+                      >
+                        {title}
+                      </a>
+                    )}
                   </motion.li>
                 );
               })}
